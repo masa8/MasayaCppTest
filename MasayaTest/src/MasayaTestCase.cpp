84c9ddc53bb3550dd1f42193a47cced68cdbf366
masaya shimomatsu <masaya.shimomatsu@gmail.com>
2012-05-21 17:57:30 +0900

    first test by eclipse

---
+++ MasayaTest/src/MasayaTestCase.cpp
@@ -0,0 +1,29 @@
+/*
+ * MasayaTestCase.cpp
+ *
+ *  Created on: 2012/05/21
+ *      Author: kevin
+ */
+
+#include "MasayaTestCase.h"
+
+CPPUNIT_TEST_SUITE_REGISTRATION( MasayaTestCase );
+
+MasayaTestCase::MasayaTestCase() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MasayaTestCase::~MasayaTestCase() {
+	// TODO Auto-generated destructor stub
+}
+void
+MasayaTestCase::setUp() {
+	// TODO Auto-generated destructor stub
+}
+
+void
+MasayaTestCase::test_test() {
+	  CPPUNIT_ASSERT (true);
+}
+
